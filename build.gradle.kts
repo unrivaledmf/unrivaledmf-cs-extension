b6d909ff11b4cc78ceb70626079b8fc242f9f65a
Muhammad Fadil <107534586+unrivaledmf@users.noreply.github.com>
2023-02-10 23:21:44 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
 
 buildscript {
@@ -36,16 +36,23 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/unrivaledmf/unrivaledmf-cs-extension")
+
+        authors = listOf("Hexated")
     }
 
     android {
-        compileSdkVersion(30)
+        compileSdkVersion(33)
 
         defaultConfig {
             minSdk = 21
-            targetSdk = 30
+            targetSdk = 33
+
+//            val properties = Properties()
+//            properties.load(project.rootProject.file("local.properties").inputStream())
+//
+//            buildConfigField("String", "API_KEY", "\"${properties.getProperty("API_KEY")}\"")
+
         }
 
         compileOptions {
@@ -76,8 +83,15 @@ subprojects {
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.3.2") // http library
-        implementation("org.jsoup:jsoup:1.13.1") // html parser
+        implementation("com.github.Blatzar:NiceHttp:0.4.2") // http library
+        implementation("org.jsoup:jsoup:1.15.3") // html parser
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        implementation("io.karn:khttp-android:0.1.2")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4") // html parser
+        //run JS
+        implementation("org.mozilla:rhino:1.7.14")
+        implementation("com.google.code.gson:gson:2.8.9")
+
     }
 }
 
