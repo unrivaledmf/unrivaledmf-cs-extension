6b5625e572877f08f3b202f802c2e1e9ffc8edfb
Muhammad Fadil <107534586+unrivaledmf@users.noreply.github.com>
2023-02-10 22:45:03 +0800

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,22 +3,25 @@ version = 1
 
 
 cloudstream {
+    language = "id"
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    // description = "Lorem Ipsum"
+    authors = listOf("fadil")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
     status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
 
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=anichin.vip&sz=%size%"
 }
